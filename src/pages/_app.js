b052d01e06db1e342e3924ac6de0bb9a49680aaf
Daniel Puscher <dev@daniel-puscher.de>
2020-03-22 10:09:44 +0100

    Add google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,12 +2,14 @@ import React from "react";
 import Head from "next/head";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import GoogleAnalytics from "../components/tracking/GoogleAnalytics";
 import GlobalStyles from "../styles/GlobalStyles";
 
 export default ({ Component, pageProps }) => (
   <>
     <Head>
       <title>PodcastPhone · Digitale Inhalte für die Strippe</title>
+      <GoogleAnalytics />
     </Head>
     <GlobalStyles />
     <Header />
