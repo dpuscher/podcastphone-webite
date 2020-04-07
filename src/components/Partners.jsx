225a3c216bff23646cea7d8eb82b802ef35c766c
JoeDotNet <40823971+JoeDotNet@users.noreply.github.com>
2020-04-07 14:19:14 +0200

    solution enabler added
    Added Solution Enabler Logo because we are getting supported.

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -4,6 +4,7 @@ import cellularLogo from "../assets/logo_cellular.svg";
 import zdfLogo from "../assets/logo_zdf.svg";
 import regierungLogo from "../assets/logo_bundesregierung.svg";
 import hackathonLogo from "../assets/logo_hackathon.png";
+import solutionEnablerLogo from "../assets/Solution-Enabler.png"
 import { Heading, Logos, LogoWrapper, Wrapper } from "./styles/Partners.styles";
 
 const Partners = () => (
@@ -20,6 +21,7 @@ const Partners = () => (
       <Logos>
         <Heading>Unterstützt von:</Heading>
         <LogoWrapper>
+          <img src={solutionEnablerLogo} width="120" />
           <img src={zdfLogo} alt="ZDF" width="120" />
           <img src={telekomLogo} alt="Telekom" width="120" />
           <img src={cellularLogo} alt="Cellular" width="120" />
